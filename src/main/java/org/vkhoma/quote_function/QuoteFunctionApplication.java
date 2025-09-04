973d1259fd11e839150750de5daf2166d8c51e97
vkhoma <16196348+HVV83@users.noreply.github.com>
2025-09-04 09:00:22 +0200

    feat(init): initialize quote function

---
+++ src/main/java/org/vkhoma/quote_function/QuoteFunctionApplication.java
@@ -0,0 +1,13 @@
+package org.vkhoma.quote_function;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoteFunctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuoteFunctionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/vkhoma/quote_function/QuoteFunctionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vkhoma.quote_function;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoteFunctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
